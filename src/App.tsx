9ce5c32bff4581fc9957b376e77e9255351b8060
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-02-08 23:46:36 +0100

    Grid and AppBar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css'
+import { Container, Typography, Grid } from '@mui/material';
+import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar/ResponsiveAppBar';
 
 function App() {
-
-
   return (
     <>
-     
+      <ResponsiveAppBar />
+      <Container>
+        <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+          <Typography variant='h1' component="h1" fontSize='clamp(1rem, 10vw, 10rem)' gutterBottom>
+            ReactWeatherApp
+          </Typography>
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Logo always on the left */}
+          <AdbIcon sx={{ display: 'flex', mr: 1 }} />
+
+          {/* Title that disappears from 'sm' and smaller screens */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#responsive-app-bar"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', sm: 'flex' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            ReactWeatherApp
+          </Typography>
+
+          {/* Display links for large screens */}
+          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          {/* Hamburger menu for smaller screens */}
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, ml: 'auto' }}>
+            <IconButton
+              size="large"
+              aria-label="navigation menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
